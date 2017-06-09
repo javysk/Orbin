1db65e00e9edac8e76a9374309accaaabb14902d
Javysk <javysk_725@hotmail.com>
2017-06-09 11:44:29 -0300

    Orbin

---
+++ Orbin/Scripts/orbin.js
@@ -0,0 +1,102 @@
+﻿$(document).ready(function () {
+
+    document.title = 'Orbin Gestión - Soluciones informáticas';
+    $(document).attr("title", "Orbin Gestión - Soluciones informáticas");
+    $('html head').find('title').text("Orbin Gestión - Soluciones informáticas");
+    $("div[style='opacity: 0.9; z-index: 2147483647; position: fixed; left: 0px; bottom: 0px; height: 65px; right: 0px; display: block; width: 100%; background-color: #202020; margin: 0px; padding: 0px;']").hide();
+    $("div[style='margin: 0px; padding: 0px; left: 0px; width: 100%; height: 65px; right: 0px; bottom: 0px; display: block; position: fixed; z-index: 2147483647; opacity: 0.9; background-color: rgb(32, 32, 32);']").hide();
+    $("div[onmouseover='S_ssac();']").hide();
+    $("center").hide();
+});
+// Closes the sidebar menu
+$("#menu-close").click(function (e) {
+    e.preventDefault();
+    $("#sidebar-wrapper").toggleClass("active");
+});
+// Opens the sidebar menu
+$("#menu-toggle").click(function (e) {
+    e.preventDefault();
+    $("#sidebar-wrapper").toggleClass("active");
+});
+// Scrolls to the selected menu item on the page
+$(function () {
+    $('a[href*=#]:not([href=#],[data-toggle],[data-target],[data-slide])').click(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
+});
+//#to-top button appears after scrolling
+var fixed = false;
+$(document).scroll(function () {
+    if ($(this).scrollTop() > 250) {
+        if (!fixed) {
+            fixed = true;
+            // $('#to-top').css({position:'fixed', display:'block'});
+            $('#to-top').show("slow", function () {
+                $('#to-top').css({
+                    position: 'fixed',
+                    display: 'block'
+                });
+            });
+        }
+    } else {
+        if (fixed) {
+            fixed = false;
+            $('#to-top').hide("slow", function () {
+                $('#to-top').css({
+                    display: 'none'
+                });
+            });
+        }
+    }
+});
+// Disable Google Maps scrolling
+// See http://stackoverflow.com/a/25904582/1607849
+// Disable scroll zooming and bind back the click event
+var onMapMouseleaveHandler = function (event) {
+    var that = $(this);
+    that.on('click', onMapClickHandler);
+    that.off('mouseleave', onMapMouseleaveHandler);
+    that.find('iframe').css("pointer-events", "none");
+}
+var onMapClickHandler = function (event) {
+    var that = $(this);
+    // Disable the click handler until the user leaves the map area
+    that.off('click', onMapClickHandler);
+    // Enable scrolling zoom
+    that.find('iframe').css("pointer-events", "auto");
+    // Handle the mouse leave event
+    that.on('mouseleave', onMapMouseleaveHandler);
+}
+// Enable map zooming with mouse scroll when the user clicks the map
+$('.map').on('click', onMapClickHandler);
+
+$(document).on('click', ".img-portfolio", function (e) {
+    e.preventDefault();
+    e.stopPropagation();
+    
+    var bb = bootbox.dialog({
+        title: "Previsualización",
+        message: '<div class="row">  ' +
+        '<div class="col-md-12"> ' +
+        '<form class="form-horizontal"> ' +
+        '<div class="form-group"> ' +
+        '<img class="' + 'previewEmbebedIMG' +
+        '" src=' + $(this).attr("src") + '> </div> </form> </div> </div>'
+    }
+    );
+    bb.css("z-index", 1000000000);
+});
+
+function ShowMoreImg(e) {
+    $(e.target).fadeOut();
+    $("#moreImages").fadeIn();
+}
